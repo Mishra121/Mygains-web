342bed29122fcfc352d0d7a40513fdca57bbbb7b
Vibhu <vibhu98mishra@gmail.com>
2020-09-15 23:52:39 +0530

    Responsive header with drawer :hammer:

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,6 +6,21 @@ import {Link} from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 // import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
+import clsx from 'clsx';
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import DescriptionOutlinedIcon from '@material-ui/icons/DescriptionOutlined';
+import LocalMallIcon from '@material-ui/icons/LocalMall';
+import LibraryBooksOutlinedIcon from '@material-ui/icons/LibraryBooksOutlined';
+import ExitToAppOutlinedIcon from '@material-ui/icons/ExitToAppOutlined';
+import PermIdentityIcon from '@material-ui/icons/PermIdentity';
+import PersonAddIcon from '@material-ui/icons/PersonAdd';
+import Hidden from '@material-ui/core/Hidden';
+import MenuIcon from '@material-ui/icons/Menu';
+import Divider from '@material-ui/core/Divider';
 
 
 const useStyles = makeStyles((theme) => ({
@@ -20,7 +35,13 @@ const useStyles = makeStyles((theme) => ({
     },
     navDiv: {
       marginBottom: 60
-    } 
+    },
+    list: {
+      width: 250,
+    },
+    fullList: {
+      width: 'auto',
+    },
   }));
 
 const Header = () => {
@@ -28,11 +49,75 @@ const Header = () => {
     const { currentUser } = useContext(AuthContext);
 
     const classes = useStyles();
+
+    const [state, setState] = React.useState({
+      right: false,
+    });
+
+    const toggleDrawer = (anchor, open) => (event) => {
+      if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+        return;
+      }
+  
+      setState({ ...state, [anchor]: open });
+    };
+
+    const list = (anchor) => (
+      <div
+        className={clsx(classes.list, {
+          [classes.fullList]: anchor === 'top' || anchor === 'bottom',
+        })}
+        role="presentation"
+        onClick={toggleDrawer(anchor, false)}
+        onKeyDown={toggleDrawer(anchor, false)}
+      >
+        <List>
+            <ListItem button component={Link} to="/plans">
+              <ListItemIcon><DescriptionOutlinedIcon></DescriptionOutlinedIcon></ListItemIcon>
+              <ListItemText primary={"Plans"}  />
+            </ListItem>
+            <ListItem button component={Link} to="/shop">
+              <ListItemIcon><LocalMallIcon></LocalMallIcon></ListItemIcon>
+              <ListItemText primary={"Shop"}  />
+            </ListItem>
+            <ListItem button component={Link} to="/blogs">
+              <ListItemIcon><LibraryBooksOutlinedIcon></LibraryBooksOutlinedIcon></ListItemIcon>
+              <ListItemText primary={"Stories"}  />
+            </ListItem>
+        </List>
+        <Divider></Divider>
+        <List>
+        {
+          !!currentUser ? (
+            <ListItem button onClick={() => app.auth().signOut()}>
+              <ListItemIcon><ExitToAppOutlinedIcon></ExitToAppOutlinedIcon></ListItemIcon>
+              <ListItemText primary={"Logout"} />
+            </ListItem>
+          ):
+          (
+            <>
+            <ListItem button component={Link} to="/login">
+              <ListItemIcon><PermIdentityIcon></PermIdentityIcon></ListItemIcon>
+              <ListItemText primary={"LogIn"} />
+            </ListItem>
+            <ListItem button component={Link} to="/signup">
+              <ListItemIcon><PersonAddIcon></PersonAddIcon></ListItemIcon>
+              <ListItemText primary={"SignUp"} />
+            </ListItem>
+            </>
+          )
+        }
+        </List>
+      </div>
+    );
+  
   
     return (
     <div className={classes.navDiv}>
+    {/* Large Screen Nav */}
     <nav className="d-flex justify-content-between navtop navbar navbar-expand-lg navbar fixed-top">
       <Link className="navbar-brand" to="/">MyGains</Link>
+      <Hidden mdDown>
       <ul className="navbar-nav">
         <li className="nav-item active">
           <Link className="nav-link" to="/plans">Plans<span className="sr-only">(current)</span></Link>
@@ -65,7 +150,18 @@ const Header = () => {
           )
         }
       </ul>
+      </Hidden>
+      <Hidden mdUp>
+        <MenuIcon  onClick={toggleDrawer('right', true)}>
+        </MenuIcon>
+      </Hidden>
     </nav>
+    {/* Small Screen Drawer */}
+    <div>
+      <Drawer anchor={'right'} open={state['right']} onClose={toggleDrawer('right', false)}>
+        {list('right')}
+      </Drawer>
+    </div>
     </div>
   );
 }
